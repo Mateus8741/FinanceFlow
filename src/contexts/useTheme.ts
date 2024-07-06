02789ff4258b573c5da52198c28d2c12a7e32548
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-06 11:30:56 -0300

    feat: Add persistent theme storage using AsyncStorage

--- src/contexts/useTheme.ts
+++ src/contexts/useTheme.ts
@@ -1,14 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type ThemeProps = {
-  theme: string;
-  setTheme: (theme: string) => void;
+  theme: 'light' | 'dark' | undefined;
+  setTheme: (theme: 'light' | 'dark' | undefined) => void;
 };
 
-const useThemeStore = create<ThemeProps>((set) => ({
-  theme: '',
-  setTheme: (theme) => set({ theme }),
-}));
+const useThemeStore = create<ThemeProps>()(
+  persist(
+    (set) => ({
+      theme: undefined,
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: 'theme-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
 
 export function useThemeStorage() {
   const { theme, setTheme } = useThemeStore();
