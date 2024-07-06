6c31efae434004a4556adb33287f528bcfde36ea
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-06 19:34:29 -0300

    refactor: Update LoginScreen to include OrView component and improve button layout

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,10 @@
 import { useColorScheme } from 'nativewind';
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Pressable, Text, View } from 'react-native';
 
 import BlurFormDark from '@/assets/BlurFormDark.png';
 import BlurFormLight from '@/assets/BlurFormLight.png';
 import { Box, CustomButton, PasswordInput, TextInput } from '@/components';
+import { OrView } from '@/components/OrView';
 
 export function LoginScreen() {
   const { colorScheme } = useColorScheme();
@@ -18,8 +19,8 @@ export function LoginScreen() {
             source={BlurFormColor}
             resizeMode="cover"
             className="rounded-full px-6 py-7">
-            <Text className="mb-4 text-center text-5xl font-bold">Login</Text>
-            <Text className="mb-5 text-center text-lg text-gray-400 dark:text-gray-300">
+            <Text className="mb-4 text-center text-5xl font-bold dark:text-white">Login</Text>
+            <Text className="dark:text-gray-200 text-center text-lg text-gray-400">
               Entre e controle suas finaças
             </Text>
 
@@ -31,6 +32,17 @@ export function LoginScreen() {
             <Text className="my-6 text-right font-bold text-blue-500">Esqueceu a senha?</Text>
 
             <CustomButton title="Entrar" />
+
+            <OrView />
+
+            <View className="flex-row items-center justify-center gap-x-2">
+              <Text className="dark:text-gray-200 text-center text-gray-400">
+                Não tem uma conta?
+              </Text>
+              <Pressable>
+                <Text className="text-center font-bold text-blue-500">Cadastre-se</Text>
+              </Pressable>
+            </View>
           </ImageBackground>
         </View>
       </View>
