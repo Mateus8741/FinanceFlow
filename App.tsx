916386a56f654f77d71645be3488089d2a2d81bc
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-06 11:31:01 -0300

    refactor: Update App.tsx to use useThemeChanger hook for theme switching

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { useThemeStorage } from '@/contexts/useTheme';
 import { LoginScreen } from '@/screens/LoginScreen';
-
+import { useThemeChanger } from '@/service';
 import './global.css';
 
 export default function App() {
+  const { theme } = useThemeStorage();
+  const { setColorScheme } = useThemeChanger();
+
+  useEffect(() => {
+    if (theme) {
+      setColorScheme(theme);
+    }
+  }, [theme]);
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <StatusBar style="auto" animated translucent />
 
-        <LoginScreen />
+        <View className="flex-1">
+          <LoginScreen />
+        </View>
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
