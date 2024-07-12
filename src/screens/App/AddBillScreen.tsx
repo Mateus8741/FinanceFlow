f3df9c91cc4777b4def062d57a319cac0c7c8a08
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-12 20:10:38 -0300

    feat: Add form inputs for creating a new bill

--- src/screens/App/AddBillScreen.tsx
+++ src/screens/App/AddBillScreen.tsx
@@ -1,11 +1,19 @@
-import { Text } from 'react-native';
-
-import { Box } from '@/components';
+import { Box, TextInput } from '@/components';
 
 export function AddBillScreen() {
   return (
     <Box>
-      <Text>AddBillScreen</Text>
+      <TextInput placeholder="Insira o nome do banco" />
+
+      <TextInput placeholder="Insira o nome da transação" />
+
+      <TextInput placeholder="Income or Outcome" />
+
+      <TextInput placeholder="pix, débito, crédito ou dinheiro" />
+
+      <TextInput placeholder="Insira o valor" />
+
+      <TextInput placeholder="Insira a data" />
     </Box>
   );
 }
