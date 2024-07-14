9dde3538180db432421c793b55eb15b08f41f642
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-13 21:30:34 -0300

    feat: Update AddBillScreen form inputs and add payment type selection

--- src/screens/App/AddBillScreen.tsx
+++ src/screens/App/AddBillScreen.tsx
@@ -1,8 +1,31 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
 
-import { Box, TextInput } from '@/components';
+import { Box, CusstomCheckbox, TextInput } from '@/components';
+import { colors } from '@/theme/colors';
+
+const data = [
+  {
+    id: 0,
+    text: 'Pix',
+  },
+  {
+    id: 1,
+    text: 'Crédito',
+  },
+  {
+    id: 2,
+    text: 'Débito',
+  },
+  {
+    id: 3,
+    text: 'Dinheiro',
+  },
+];
 
 export function AddBillScreen() {
+  const [selectedTable, setSelectedTable] = useState<any>();
+
   return (
     <Box>
       <View className="flex-1 justify-center gap-6">
@@ -12,7 +35,28 @@ export function AddBillScreen() {
 
         <TextInput label="Income/Outcome" placeholder="Income or Outcome" />
 
-        <TextInput label="Tipo de pagamento" placeholder="pix, débito, crédito ou dinheiro" />
+        <Text className="text-lg font-bold text-black dark:text-white">Tipo de pagamento</Text>
+
+        <View className="flex-row justify-around">
+          {data.map((item) => (
+            <View key={item.id} className="items-center justify-center">
+              <CusstomCheckbox
+                fillColor={
+                  selectedTable && selectedTable.id === item.id ? colors.white : colors.blue[300]
+                }
+                isChecked={selectedTable === item}
+                onPress={() => {
+                  setSelectedTable(item);
+                  console.log(item);
+                }}
+              />
+
+              <Text className="mt-2 text-base font-bold text-black dark:text-white">
+                {item.text}
+              </Text>
+            </View>
+          ))}
+        </View>
 
         <TextInput label="Valor" placeholder="Insira o valor" />
 
