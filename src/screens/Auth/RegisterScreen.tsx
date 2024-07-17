f1782cf1758d2aad72a0115b5b0188f6feb75246
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-16 21:44:58 -0300

    feat: Add birth date formatting to RegisterScreen

--- src/screens/Auth/RegisterScreen.tsx
+++ src/screens/Auth/RegisterScreen.tsx
@@ -1,16 +1,29 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useColorScheme } from 'nativewind';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { ImageBackground, Pressable, Text, View } from 'react-native';
 
 import { AuthScreenProps } from '@/Routes';
 import BlurFormDark from '@/assets/BlurFormDark.png';
 import BlurFormLight from '@/assets/BlurFormLight.png';
-import { Box, CustomButton, FormPasswordInput, FormTextInput } from '@/components';
+import { Box, CustomButton, FormPasswordInput, FormTextInput, TextInput } from '@/components';
 import { OrView } from '@/components/OrView';
 import { registerScheema, RegisterScheema } from '@/schemas';
 import { colors } from '@/theme/colors';
+
+function formatBirthDate(value: string) {
+  // Remove qualquer caractere que não seja número
+  value = value.replace(/\D/g, '');
+
+  if (value.length > 2) {
+    value = value.slice(0, 2) + '/' + value.slice(2);
+  }
+  if (value.length > 5) {
+    value = value.slice(0, 5) + '/' + value.slice(5, 9);
+  }
+  return value;
+}
 
 export function RegisterScreen({ navigation }: AuthScreenProps<'RegisterScreen'>) {
   const { control, handleSubmit } = useForm({
@@ -56,20 +69,36 @@ export function RegisterScreen({ navigation }: AuthScreenProps<'RegisterScreen'>
             <View className="my-6 gap-y-5">
               <FormTextInput control={control} name="name" placeholder="Nome" />
               <FormTextInput control={control} name="lastName" placeholder="Sobrenome" />
-              <FormTextInput control={control} name="email" placeholder="E-mail" />
               <FormTextInput
                 control={control}
-                name="birthDate"
-                placeholder="Data de nascimento"
-                rightComponent={
-                  <MaterialCommunityIcons
-                    name="calendar-account-outline"
-                    size={24}
-                    color={colors.gray[400]}
-                    opacity={0.5}
-                  />
-                }
+                name="email"
+                placeholder="E-mail"
+                keyboardType="email-address"
               />
+
+              <Controller
+                control={control}
+                name="birthDate"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    placeholder="Data de nascimento"
+                    onBlur={onBlur}
+                    onChangeText={(value) => onChange(formatBirthDate(value))}
+                    value={value}
+                    keyboardType="numeric"
+                    maxLength={10}
+                    rightComponent={
+                      <MaterialCommunityIcons
+                        name="calendar-account-outline"
+                        size={24}
+                        color={colors.gray[400]}
+                        opacity={0.5}
+                      />
+                    }
+                  />
+                )}
+              />
+
               <FormPasswordInput control={control} name="password" placeholder="Senha" />
               <FormPasswordInput
                 control={control}
