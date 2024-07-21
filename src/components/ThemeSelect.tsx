5ea28e585bce9505a5c08d81c25336b6a6147090
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-21 20:32:54 -0300

    feat: Add selected theme icon to ThemeSelect component

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -1,4 +1,4 @@
-import { Pressable, PressableProps, Text } from 'react-native';
+import { Pressable, PressableProps, Text, View } from 'react-native';
 
 import { Icon } from './Icons/CustonIcons';
 
@@ -13,11 +13,25 @@ interface ThemeSelectProps extends PressableProps {
 export function ThemeSelect({ label, selectedTheme, themeValue, ...props }: ThemeSelectProps) {
   const isSelected = selectedTheme === themeValue;
 
+  const iconBySelectedTheme: { [key: string]: string } = {
+    light: 'Sun',
+    dark: 'Moon',
+    system: 'Settings',
+  };
+
+  const selectedIcon = themeValue ? iconBySelectedTheme[themeValue] : '';
+
   return (
     <Pressable className="flex-row items-center justify-between gap-x-2" hitSlop={10} {...props}>
       <Text className="text-lg font-bold text-black dark:text-white">{label}</Text>
 
-      {isSelected && <Icon icon="Check" size={25} color={colors.blue[500]} />}
+      <View className="flex-row items-center gap-x-2">
+        {isSelected && <Icon icon="Check" size={25} color={colors.blue[500]} />}
+
+        <View className="items-center justify-center rounded-xl bg-blue-200 p-2 dark:bg-blue-700">
+          <Icon icon={selectedIcon as any} size={25} color={colors.blue[500]} />
+        </View>
+      </View>
     </Pressable>
   );
 }
