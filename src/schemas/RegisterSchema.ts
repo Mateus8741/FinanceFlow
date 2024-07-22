6b935dad722ea81a29e2c39925875a4b5e36eb5d
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-22 06:17:12 -0300

    refactor: Remove minimum password length requirement for confirmPassword field in RegisterSchema

--- src/schemas/RegisterSchema.ts
+++ src/schemas/RegisterSchema.ts
@@ -7,7 +7,7 @@ export const registerScheema = z
     email: z.string().email('Endereço de e-mail inválido'),
     birthDate: z.string().min(10, 'Data de nascimento inválida'),
     password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
-    confirmPassword: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
+    confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'As senhas não coincidem',
