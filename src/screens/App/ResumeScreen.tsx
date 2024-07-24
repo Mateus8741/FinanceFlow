4bbd3f104fac7779859ef251dbc2d2fa363032a6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-24 13:48:23 -0300

    refactor: Update ResumeScreen to display income and outcome payment totals

--- src/screens/App/ResumeScreen.tsx
+++ src/screens/App/ResumeScreen.tsx
@@ -1,30 +1,37 @@
 import { useLayoutEffect } from 'react';
-import { FlatList } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { Box, HeaderData, ResumeCard } from '@/components';
 import { usePaymentTotals } from '@/service';
 
 export function ResumeScreen() {
-  const { cash, credit, debit, pix, refetch } = usePaymentTotals();
+  const { income, outcome, refetch } = usePaymentTotals();
 
   useLayoutEffect(() => {
     refetch();
-  });
+  }, [refetch]);
 
   return (
     <Box>
       <HeaderData title="Relatório" subtitle="detalhado" />
 
-      <FlatList
-        data={[1, 2, 3, 4, 5]}
-        keyExtractor={(item) => String(item)}
-        renderItem={() => <ResumeCard type="Pix" value={1320} />}
-      />
+      <View className="mt-2">
+        <Text className="mb-2 text-lg font-bold text-green-800">Resumo de entrada</Text>
 
-      <ResumeCard type="Pix" value={pix} />
-      <ResumeCard type="Crédito" value={credit} />
-      <ResumeCard type="Débito" value={debit} />
-      <ResumeCard type="Dinheiro" value={cash} />
+        <ResumeCard type="Pix" value={income.pix} />
+        <ResumeCard type="Crédito" value={income.credit} />
+        <ResumeCard type="Débito" value={income.debit} />
+        <ResumeCard type="Dinheiro" value={income.cash} />
+      </View>
+
+      <View className="mt-2">
+        <Text className="mb-2 text-lg font-bold text-red-500">Resumo de saída</Text>
+
+        <ResumeCard type="Pix" value={outcome.pix} />
+        <ResumeCard type="Crédito" value={outcome.credit} />
+        <ResumeCard type="Débito" value={outcome.debit} />
+        <ResumeCard type="Dinheiro" value={outcome.cash} />
+      </View>
     </Box>
   );
 }
