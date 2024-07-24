47ac9561112b9dfe7ceff6297fc22dd41a806b63
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-24 18:19:39 -0300

    refactor: Update ResumeCard to display payment type and value

--- src/components/ResumeCard.tsx
+++ src/components/ResumeCard.tsx
@@ -4,7 +4,7 @@ import { tv } from 'tailwind-variants';
 import { FormatCurrency } from '@/utils';
 
 const resumeStyles = tv({
-  base: 'mb-4 flex-row justify-between rounded-md border-l-4 bg-white p-4 shadow-sm dark:bg-glassDark',
+  base: 'mb-4 flex-col justify-between rounded-md border-l-4 bg-white py-2 px-3 shadow-sm dark:bg-glassDark w-full h-16',
   variants: {
     cardType: {
       Pix: 'border-l-blue-550',
@@ -18,15 +18,21 @@ const resumeStyles = tv({
 interface ResumeCardProps {
   type: 'Pix' | 'Dinheiro' | 'Crédito' | 'Débito';
   value: number;
+  isOutcome?: boolean;
 }
 
-export function ResumeCard({ type, value }: ResumeCardProps) {
+export function ResumeCard({ type, value, isOutcome = true }: ResumeCardProps) {
   const formattedValue = FormatCurrency(value);
+
+  const textColor = isOutcome ? 'text-red-500' : 'text-green-500';
 
   return (
     <View className={resumeStyles({ cardType: type })}>
-      <Text className="text-base font-bold text-black dark:text-white">{type}</Text>
-      <Text className="text-base font-bold text-black dark:text-white">{formattedValue}</Text>
+      <Text className="text-sm font-bold text-black dark:text-white">{type}</Text>
+      <Text className={`text-md font-bold ${textColor}`}>
+        {isOutcome ? '- ' : ''}
+        {formattedValue}
+      </Text>
     </View>
   );
 }
