002985dce7a01d988d22b35bb0bd1a43589bec8c
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-24 14:18:13 -0300

    refactor: Update calcPaymentType to calculate income and outcome payment totals

--- src/service/calcPaymentType.ts
+++ src/service/calcPaymentType.ts
@@ -1,72 +1,63 @@
-import { useMemo } from 'react';
-
 import { useGetTransactions } from '@/api';
 
 function PaymentTotals() {
-  const { transaction, refetch } = useGetTransactions();
+  const { transaction } = useGetTransactions();
 
-  const totals = useMemo(() => {
-    const initialTotals = {
-      income: { debit: 0, credit: 0, cash: 0, pix: 0 },
-      outcome: { debit: 0, credit: 0, cash: 0, pix: 0 },
-    };
+  const initialTotals = {
+    income: { debit: 0, credit: 0, cash: 0, pix: 0 },
+    outcome: { debit: 0, credit: 0, cash: 0, pix: 0 },
+  };
 
-    return (
-      transaction?.reduce((acc, item) => {
-        if (item.value !== null) {
-          if (item.transacion_type === 'income') {
-            switch (item.payment_type) {
-              case 'Débito':
-                acc.income.debit += item.value;
-                break;
-              case 'Crédito':
-                acc.income.credit += item.value;
-                break;
-              case 'Dinheiro':
-                acc.income.cash += item.value;
-                break;
-              case 'Pix':
-                acc.income.pix += item.value;
-                break;
-              default:
-                break;
-            }
-          } else if (item.transacion_type === 'outcome') {
-            switch (item.payment_type) {
-              case 'Débito':
-                acc.outcome.debit += item.value;
-                break;
-              case 'Crédito':
-                acc.outcome.credit += item.value;
-                break;
-              case 'Dinheiro':
-                acc.outcome.cash += item.value;
-                break;
-              case 'Pix':
-                acc.outcome.pix += item.value;
-                break;
-              default:
-                break;
-            }
+  const totals =
+    transaction?.reduce((acc, item) => {
+      if (item.value !== null) {
+        if (item.transacion_type === 'income') {
+          switch (item.payment_type) {
+            case 'Débito':
+              acc.income.debit += item.value;
+              break;
+            case 'Crédito':
+              acc.income.credit += item.value;
+              break;
+            case 'Dinheiro':
+              acc.income.cash += item.value;
+              break;
+            case 'Pix':
+              acc.income.pix += item.value;
+              break;
+            default:
+              break;
+          }
+        } else if (item.transacion_type === 'outcome') {
+          switch (item.payment_type) {
+            case 'Débito':
+              acc.outcome.debit += item.value;
+              break;
+            case 'Crédito':
+              acc.outcome.credit += item.value;
+              break;
+            case 'Dinheiro':
+              acc.outcome.cash += item.value;
+              break;
+            case 'Pix':
+              acc.outcome.pix += item.value;
+              break;
+            default:
+              break;
           }
         }
-        return acc;
-      }, initialTotals) || initialTotals
-    );
-  }, [transaction]);
+      }
+      return acc;
+    }, initialTotals) || initialTotals;
 
-  return {
-    ...totals,
-    refetch,
-  };
+  return totals;
 }
 
 export function usePaymentTotals() {
-  const { income, outcome, refetch } = PaymentTotals();
+  const { income, outcome } = PaymentTotals();
 
   return {
     income,
     outcome,
-    refetch,
   };
 }
