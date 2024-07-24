407e2ddf70c57774375cf1b6bec933d707ee0cbd
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-24 11:37:44 -0300

    refactor: Add payment totals to ResumeScreen

--- src/screens/App/ResumeScreen.tsx
+++ src/screens/App/ResumeScreen.tsx
@@ -1,8 +1,11 @@
 import { FlatList } from 'react-native';
 
 import { Box, HeaderData, ResumeCard } from '@/components';
+import { usePaymentTotals } from '@/service';
 
 export function ResumeScreen() {
+  const { cash, credit, debit, pix } = usePaymentTotals();
+
   return (
     <Box>
       <HeaderData title="Relatório" subtitle="detalhado" />
@@ -10,8 +13,13 @@ export function ResumeScreen() {
       <FlatList
         data={[1, 2, 3, 4, 5]}
         keyExtractor={(item) => String(item)}
-        renderItem={() => <ResumeCard title="Saldo" value={1320} />}
+        renderItem={() => <ResumeCard type="Pix" value={1320} />}
       />
+
+      <ResumeCard type="Pix" value={pix} />
+      <ResumeCard type="Crédito" value={credit} />
+      <ResumeCard type="Débito" value={debit} />
+      <ResumeCard type="Dinheiro" value={cash} />
     </Box>
   );
 }
