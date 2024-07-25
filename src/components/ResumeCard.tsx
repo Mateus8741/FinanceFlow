77c609566094179a9ae6bcf28621512163003931
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-25 18:34:09 -0300

    refactor: Update ResumeCard to display payment type and value

--- src/components/ResumeCard.tsx
+++ src/components/ResumeCard.tsx
@@ -4,7 +4,7 @@ import { tv } from 'tailwind-variants';
 import { FormatCurrency } from '@/utils';
 
 const resumeStyles = tv({
-  base: 'mb-4 flex-col justify-between rounded-md border-l-4 bg-white py-2 px-3 shadow-sm dark:bg-glassDark w-full h-16',
+  base: 'mb-4 flex-row items-center justify-between rounded-md border-l-4 bg-white py-2 px-3 shadow-sm dark:bg-glassDark w-full h-16',
   variants: {
     cardType: {
       Pix: 'border-l-blue-550',
@@ -28,8 +28,8 @@ export function ResumeCard({ type, value, isOutcome = true }: ResumeCardProps) {
 
   return (
     <View className={resumeStyles({ cardType: type })}>
-      <Text className="text-sm font-bold text-black dark:text-white">{type}</Text>
-      <Text className={`text-md font-bold ${textColor}`}>
+      <Text className="text-lg font-bold text-black dark:text-white">{type}</Text>
+      <Text className={`text-lg font-bold ${textColor}`}>
         {isOutcome ? '- ' : ''}
         {formattedValue}
       </Text>
