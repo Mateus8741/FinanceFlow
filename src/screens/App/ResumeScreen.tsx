c25d8d263e787bfc3922def16c2749a38111ba59
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-29 13:21:57 -0300

    refactor: Update ResumeScreen to display income and outcome payment totals

--- src/screens/App/ResumeScreen.tsx
+++ src/screens/App/ResumeScreen.tsx
@@ -30,28 +30,35 @@ export function ResumeScreen() {
 
       <SelectResume setListType={setListType} />
 
-      <Chart data={incoming} />
-      {/* <Chart data={outcoming} /> */}
+      {listType === 'income' ? (
+        <>
+          <Chart data={incoming} />
 
-      <View className="flex-row gap-x-4">
-        <View className="flex-1">
-          <Text className="mb-2 text-lg font-bold text-green-500">Entrada</Text>
+          <View className="flex-row gap-x-4">
+            <View className="flex-1">
+              <Text className="mb-2 text-lg font-bold text-green-500">Entrada</Text>
 
-          <ResumeCard type="Pix" value={income.pix} isOutcome={false} />
-          <ResumeCard type="Crédito" value={income.credit} isOutcome={false} />
-          <ResumeCard type="Débito" value={income.debit} isOutcome={false} />
-          <ResumeCard type="Dinheiro" value={income.cash} isOutcome={false} />
-        </View>
+              <ResumeCard type="Pix" value={income.pix} isOutcome={false} />
+              <ResumeCard type="Crédito" value={income.credit} isOutcome={false} />
+              <ResumeCard type="Débito" value={income.debit} isOutcome={false} />
+              <ResumeCard type="Dinheiro" value={income.cash} isOutcome={false} />
+            </View>
+          </View>
+        </>
+      ) : (
+        <>
+          <Chart data={outcoming} />
 
-        {/* <View className="flex-1">
-          <Text className="mb-2 text-lg font-bold text-red-500">Saída</Text>
+          <View className="flex-1">
+            <Text className="mb-2 text-lg font-bold text-red-500">Saída</Text>
 
-          <ResumeCard type="Pix" value={outcome.pix} />
-          <ResumeCard type="Crédito" value={outcome.credit} />
-          <ResumeCard type="Débito" value={outcome.debit} />
-          <ResumeCard type="Dinheiro" value={outcome.cash} />
-        </View> */}
-      </View>
+            <ResumeCard type="Pix" value={outcome.pix} />
+            <ResumeCard type="Crédito" value={outcome.credit} />
+            <ResumeCard type="Débito" value={outcome.debit} />
+            <ResumeCard type="Dinheiro" value={outcome.cash} />
+          </View>
+        </>
+      )}
     </Box>
   );
 }
