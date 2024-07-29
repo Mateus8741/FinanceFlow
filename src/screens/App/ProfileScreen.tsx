5fdd08f3ff9be114545bae2e2e9e8f40ee070e3a
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-29 16:16:45 -0300

    refactor: Update ProfileScreen to display user's full name as avatar label

--- src/screens/App/ProfileScreen.tsx
+++ src/screens/App/ProfileScreen.tsx
@@ -1,20 +1,22 @@
 import { Image, Pressable, ScrollView, Text, View } from 'react-native';
 
 import { useLogOutUser } from '@/api';
-import defaultProfile from '@/assets/defaultProfile.jpeg';
 import { Box, Icon, ProfileMenu } from '@/components';
+import { useUserStorage } from '@/contexts';
 import { colors } from '@/theme/colors';
 
 export function ProfileScreen() {
+  const { user } = useUserStorage();
   const { logout } = useLogOutUser();
 
   function handleLogOut() {
     logout();
   }
 
-  const hasImage = true;
-
-  const uri = 'https://avatars.githubusercontent.com/u/39889384?v=4';
+  // const uri = 'https://avatars.githubusercontent.com/u/39889384?v=4';
+  const name = `${user?.user_metadata.first_name}+${user?.user_metadata.last_name}`;
+  const uri = `https://ui-avatars.com/api/?name=${name}&background=random`;
+  const fullName = `${user?.user_metadata.first_name} ${user?.user_metadata.last_name}`;
 
   return (
     <Box>
@@ -37,9 +39,10 @@ export function ProfileScreen() {
         </View>
 
         <View className="items-center justify-center rounded-xl bg-white p-4 shadow-sm dark:bg-glassDark">
-          <Image source={hasImage ? { uri } : defaultProfile} className="h-32 w-32 rounded-full" />
+          <Image source={{ uri }} alt="avatar" className="h-32 w-32 rounded-full" />
 
-          <Text className="mt-3 text-xl font-bold text-blue-500">Alterar foto</Text>
+          {/* <Text className="mt-3 text-xl font-bold text-blue-500">Alterar foto</Text> */}
+          <Text className="mt-3 text-xl font-bold text-blue-500">{fullName}</Text>
         </View>
 
         <ProfileMenu />
