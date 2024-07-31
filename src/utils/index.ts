3085e82e239ad6d51a1e9e7bb103631e4463ecfe
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-31 15:40:20 -0300

    feat: Add listBanks utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './dataMenuProfile';
 export * from './formatCurrency';
 export * from './formatCurrencyOnDigiting';
+export * from './listBanks';
 export * from './regexBirthday';
 export * from './shadowProps';
 
