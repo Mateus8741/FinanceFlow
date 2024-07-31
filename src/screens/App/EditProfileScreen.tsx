e65fe51fd9982fb075d1d9c5710667b5c76f5925
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-31 15:06:48 -0300

    refactor: Update EditProfileScreen to use useUpdateProfile hook and UpdateScheema

--- src/screens/App/EditProfileScreen.tsx
+++ src/screens/App/EditProfileScreen.tsx
@@ -4,31 +4,33 @@ import { useForm } from 'react-hook-form';
 import { Pressable, Text, View } from 'react-native';
 
 import { AppScreenProps } from '@/Routes';
-import { UseApi } from '@/api';
-import { Box, CustomButton, FormPasswordInput, FormTextInput, Icon } from '@/components';
+import { useUpdateProfile } from '@/api';
+import { Box, CustomButton, FormTextInput, Icon } from '@/components';
 import { useUserStorage } from '@/contexts';
 import { UpdateScheema, updateScheema } from '@/schemas';
 import { colors } from '@/theme/colors';
 
 export function EditProfileScreen({ navigation }: AppScreenProps<'EditProfileScreen'>) {
-  // const { update } = useUpdateProfile();
-  const { UpdateProfile } = UseApi();
   const { user } = useUserStorage();
+  const { update, isPending } = useUpdateProfile();
+  const { colorScheme } = useColorScheme();
 
-  const { control, handleSubmit } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm({
     resolver: zodResolver(updateScheema),
 
     defaultValues: {
-      name: '',
-      lastName: '',
-      email: '',
-      birthDate: '',
-      password: '',
-      confirmPassword: '',
+      first_name: user?.user_metadata.first_name,
+      last_name: user?.user_metadata.last_name,
+      email: user?.user_metadata.email,
+      birth_date: user?.user_metadata.birth_date,
     },
-  });
 
-  const { colorScheme } = useColorScheme();
+    mode: 'onChange',
+  });
 
   const iconColor = colorScheme === 'dark' ? colors.gray.bgLight : colors.gray.bg;
 
@@ -37,11 +39,10 @@ export function EditProfileScreen({ navigation }: AppScreenProps<'EditProfileScr
   }
 
   function handleSaveChanges(data: UpdateScheema) {
-    console.log(data);
-    UpdateProfile({
-      email: data.email,
+    update({
       first_name: data.first_name,
       last_name: data.last_name,
+      email: data.email,
       birth_date: data.birth_date,
     });
   }
@@ -60,28 +61,20 @@ export function EditProfileScreen({ navigation }: AppScreenProps<'EditProfileScr
             <Text className="mb-4 text-2xl font-bold text-blue-500">Dados pessoais</Text>
 
             <View className="gap-4">
-              <FormTextInput control={control} name="name" placeholder="Nome" />
-              <FormTextInput control={control} name="lastName" placeholder="Sobrenome" />
-              <FormTextInput control={control} name="birthDate" placeholder="Data de Nascimento" />
+              <FormTextInput control={control} name="first_name" placeholder="Nome" />
+              <FormTextInput control={control} name="last_name" placeholder="Sobrenome" />
+              <FormTextInput control={control} name="birth_date" placeholder="Data de Nascimento" />
               <FormTextInput control={control} name="email" placeholder="E-mail" />
-            </View>
-          </View>
-
-          <View className="items-cente mt-7 rounded-xl bg-white p-4 shadow-sm dark:bg-glassDark">
-            <Text className="mb-4 text-2xl font-bold text-blue-500">Senha</Text>
-
-            <View className="gap-4">
-              <FormPasswordInput control={control} name="password" placeholder="Senha" />
-              <FormPasswordInput
-                control={control}
-                name="confirmPassword"
-                placeholder="Confirmar Senha"
-              />
             </View>
           </View>
         </View>
 
-        <CustomButton title="Salvar alterações" onPress={handleSubmit(handleSaveChanges)} />
+        <CustomButton
+          title="Salvar alterações"
+          onPress={handleSubmit(handleSaveChanges)}
+          isDisabled={!isDirty || !isValid}
+          isLoading={isPending}
+        />
       </Box>
     </>
   );
