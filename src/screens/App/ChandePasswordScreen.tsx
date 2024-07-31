49213b0f001faf6e2604e7dc3ee4d9027f768682
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-31 15:06:35 -0300

    refactor: Update ChangePasswordScreen component to include password change functionality

--- src/screens/App/ChandePasswordScreen.tsx
+++ src/screens/App/ChandePasswordScreen.tsx
@@ -1,34 +1,77 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useColorScheme } from 'nativewind';
 import { useForm } from 'react-hook-form';
-import { Text, View } from 'react-native';
+import { Alert, Pressable, Text, View } from 'react-native';
 
-import { Box, FormPasswordInput } from '@/components';
-import { updateScheema } from '@/schemas';
+import { AppScreenProps } from '@/Routes';
+import { useUpdateProfile } from '@/api';
+import { Box, CustomButton, FormPasswordInput, Icon } from '@/components';
+import { UpdateScheema, updateScheema } from '@/schemas';
+import { colors } from '@/theme/colors';
 
-export function ChangePasswordScreen() {
-  const { control } = useForm({
+export function ChangePasswordScreen({ navigation }: AppScreenProps<'ChangePasswordScreen'>) {
+  const { update, isPending } = useUpdateProfile();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm({
     resolver: zodResolver(updateScheema),
 
     defaultValues: {
       password: '',
       confirmPassword: '',
     },
+
+    mode: 'onChange',
   });
+
+  const { colorScheme } = useColorScheme();
+
+  const iconColor = colorScheme === 'dark' ? colors.gray.bgLight : colors.gray.bg;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  function handleUpdatePassword({ password, confirmPassword }: UpdateScheema) {
+    if (password !== confirmPassword) {
+      return Alert.alert('As senhas não coincidem');
+    }
+
+    update({ password });
+  }
 
   return (
     <Box>
-      <View className="items-cente mt-7 rounded-xl bg-white p-4 shadow-sm dark:bg-glassDark">
-        <Text className="mb-4 text-2xl font-bold text-blue-500">Senha</Text>
+      <Pressable
+        className="h-6 w-6 items-center justify-center rounded-xl bg-white p-6 shadow-sm dark:bg-glassDark"
+        onPress={handleGoBack}>
+        <Icon icon="ArrowLeft" size={22} color={iconColor} />
+      </Pressable>
 
-        <View className="gap-4">
-          <FormPasswordInput control={control} name="password" placeholder="Senha" />
-          <FormPasswordInput
-            control={control}
-            name="confirmPassword"
-            placeholder="Confirmar Senha"
-          />
+      <View className="flex-1">
+        <View className="items-cente mt-7 rounded-xl bg-white p-4 shadow-sm dark:bg-glassDark">
+          <Text className="mb-4 text-2xl font-bold text-blue-500">Senha</Text>
+
+          <View className="gap-4">
+            <FormPasswordInput control={control} name="password" placeholder="Senha" />
+            <FormPasswordInput
+              control={control}
+              name="confirmPassword"
+              placeholder="Confirmar Senha"
+            />
+          </View>
         </View>
       </View>
+
+      <CustomButton
+        title="Mudar senha"
+        onPress={handleSubmit(handleUpdatePassword)}
+        isDisabled={!isDirty || !isValid}
+        isLoading={isPending}
+      />
     </Box>
   );
 }
