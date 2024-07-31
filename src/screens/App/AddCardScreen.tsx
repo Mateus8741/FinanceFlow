919acf2305805ebd93236c9022fdb49fee658717
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-31 17:24:36 -0300

    feat: Add AddCardScreen component

---
+++ src/screens/App/AddCardScreen.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { Box } from '@/components';
+
+export function AddCardScreen() {
+  return (
+    <Box>
+      <Text>Adicionar cartão</Text>
+    </Box>
+  );
+}
