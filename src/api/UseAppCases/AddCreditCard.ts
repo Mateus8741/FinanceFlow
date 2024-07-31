5bd5991120ae37f12d54d3e96344d512400d19ae
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-07-31 18:26:25 -0300

    feat: Add useAddCreditCard hook for adding credit cards

---
+++ src/api/UseAppCases/AddCreditCard.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { AddCardProps, UseApi } from '../UseApi';
+
+export function useAddCreditCard() {
+  const { AddCard } = UseApi();
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isSuccess, isPending, error } = useMutation({
+    mutationFn: (data: AddCardProps) => AddCard(data),
+    onSuccess: () => {
+      //   queryClient.invalidateQueries({ queryKey: ['all-transactions'] });
+      console.log('Card added');
+    },
+    onSettled(data, error, variables, context) {
+      if (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  return { addCard: mutate, isSuccess, isPending, error };
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,7 @@ export * from './UseAuthCases/ResetPassword';
 export * from './UseAuthCases/SignOutUser';
 
 export * from './UseAppCases/AddBill';
+export * from './UseAppCases/AddCreditCard';
 export * from './UseAppCases/GetTransactions';
 export * from './UseAppCases/UpdateProfile';
 
