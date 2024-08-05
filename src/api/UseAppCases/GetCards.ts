d58f1300536eb20a637661d8879f1f6a7a061c19
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-08-05 14:55:41 -0300

    feat: Add useGetCards hook for fetching user cards

---
+++ src/api/UseAppCases/GetCards.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { UseApi } from '../UseApi';
+
+import { useUserStorage } from '@/contexts';
+
+export function useGetCards() {
+  const { user } = useUserStorage();
+  const { GetCards } = UseApi();
+
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['all-cards'],
+    queryFn: async () => GetCards(user),
+    persister(queryFn, context, query) {
+      return queryFn(context);
+    },
+  });
+
+  return {
+    cards: data,
+    error,
+    isLoading,
+    refetch,
+  };
+}
