50c6aeec0a291f6db2f6874c0519fbf4d722cd59
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-08-21 14:20:49 -0300

    feat: Add SVG logos for Abc, Alfa, BDB, Bmg, Bradesco, Btg, Bv, Caixa, Daycoval, Fibra, Inter, Itau, Mercantil, Modal, Neon, Nubank, Original, Pan, Pine, Rendimento, Safra, Santander, and Sofisa banks

---
+++ src/assets/mapLogoBank.ts
@@ -0,0 +1,49 @@
+import Abc from './Logos/Abc.svg';
+import Alfa from './Logos/Alfa.svg';
+import BDB from './Logos/BDB.svg';
+import Bmg from './Logos/Bmg.svg';
+import Bradesco from './Logos/Bradesco.svg';
+import Btg from './Logos/Btg.svg';
+import Bv from './Logos/Bv.svg';
+import Caixa from './Logos/Caixa.svg';
+import Daycoval from './Logos/Daycoval.svg';
+import Fibra from './Logos/Fibra.svg';
+import Inter from './Logos/Inter.svg';
+import Itau from './Logos/Itau.svg';
+import Mercantil from './Logos/Mercantil.svg';
+import Modal from './Logos/Modal.svg';
+import Neon from './Logos/Neon.svg';
+import Nubank from './Logos/Nubank.svg';
+import Original from './Logos/Original.svg';
+import Pan from './Logos/Pan.svg';
+import Pine from './Logos/Pine.svg';
+import Rendimento from './Logos/Rendimento.svg';
+import Safra from './Logos/Safra.svg';
+import Santander from './Logos/Santander.svg';
+import Sofisa from './Logos/Sofisa.svg';
+
+export const BankLogos = {
+  Abc,
+  Alfa,
+  BDB,
+  Bmg,
+  Bradesco,
+  Btg,
+  Bv,
+  Caixa,
+  Daycoval,
+  Fibra,
+  Inter,
+  Itau,
+  Mercantil,
+  Modal,
+  Neon,
+  Nubank,
+  Original,
+  Pan,
+  Pine,
+  Rendimento,
+  Safra,
+  Santander,
+  Sofisa,
+};
