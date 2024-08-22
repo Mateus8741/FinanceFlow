552c0db5eafc70796918821c59bd7109a70c3c23
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-08-22 10:11:25 -0300

    refactor: Update BillCard component to display current value and due date

--- src/components/BillCard.tsx
+++ src/components/BillCard.tsx
@@ -1,57 +1,40 @@
-import { useColorScheme } from 'nativewind';
 import { Text, View } from 'react-native';
 
-import { Icon } from './Icons/CustonIcons';
+import { BankLogo } from './BankLogo';
 
-import { colors } from '@/theme/colors';
 import { FormatCurrency } from '@/utils';
 
 interface BillCardProps {
   accountName: string;
   totalValue: number;
+  currentValue: number;
+  dueDate: string;
 }
 
-export function BillCard({ accountName, totalValue }: BillCardProps) {
-  const { colorScheme } = useColorScheme();
-
-  const colorIcon = colorScheme === 'dark' ? colors.white : colors.black;
-
+export function BillCard({ accountName, totalValue, currentValue, dueDate }: BillCardProps) {
   const formattedTotalValue = FormatCurrency(totalValue);
+  const formattedCurrentValue = FormatCurrency(currentValue);
 
   return (
-    // <View className="mt-2.5 rounded-xl bg-white p-4 shadow-sm dark:bg-gray-800">
-    //   <View className="flex-row items-center gap-2">
-    //     <Icon icon="Receipt" size={24} color={colorIcon} />
+    <View className="mt-2.5 rounded-xl bg-white p-4 shadow-sm dark:bg-gray-800">
+      <View className="flex-row items-center gap-2">
+        <BankLogo bankName={accountName} />
 
-    //     <Text className="text-lg text-black dark:text-white">Fatura atual</Text>
-    //   </View>
-
-    //   <View className="mt-4">
-    //     <View className="flex-row items-center justify-between">
-    //       <Text className="text-lg text-black dark:text-white">R$ 500,00</Text>
-    //       <Text className="text-sm text-gray-400 dark:text-gray-600">Vencimento 10/10</Text>
-    //     </View>
-
-    //     <View className="mt-3 h-px bg-gray-200 dark:bg-gray-700" />
-
-    //     <View className="mt-3 flex-row items-center justify-between">
-    //       <Text className="text-sm text-gray-400 dark:text-gray-600">Limite total</Text>
-    //       <Text className="text-sm text-gray-400 dark:text-gray-600">R$ 1000,00</Text>
-    //     </View>
-    //   </View>
-    // </View>
-
-    <View className="mt-3.5 flex-row items-center gap-2">
-      <View className="h-12 w-12 items-center justify-center rounded-full bg-gray-100 dark:bg-gray-700">
-        <Icon icon="Landmark" size={22} color={colorIcon} />
+        <Text className="text-lg text-black dark:text-white">{accountName}</Text>
       </View>
 
-      <Text className="text-md flex-1 font-bold text-black dark:text-white">{accountName}</Text>
+      <View className="mt-4">
+        <View className="flex-row items-center justify-between">
+          <Text className="text-lg text-black dark:text-white">{formattedCurrentValue}</Text>
+          <Text className="text-sm text-gray-400 dark:text-gray-600">Vencimento {dueDate}</Text>
+        </View>
 
-      <View className="gap-y-2">
-        <Text className="text-md text-right text-black dark:text-white">saldo de</Text>
+        <View className="mt-3 h-px bg-gray-200 dark:bg-gray-700" />
 
-        <Text className="text-md font-bold text-black dark:text-white">{formattedTotalValue}</Text>
+        <View className="mt-3 flex-row items-center justify-between">
+          <Text className="text-sm text-gray-400 dark:text-gray-600">Limite total</Text>
+          <Text className="text-sm text-gray-400 dark:text-gray-600">{formattedTotalValue}</Text>
+        </View>
       </View>
     </View>
   );
