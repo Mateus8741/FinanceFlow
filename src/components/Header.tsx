ac4294646d85d70f551634726b6c230047d1f694
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-01 18:35:12 -0300

    refactor: Update Header component to display user's full name and avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,23 +11,23 @@ export function Header() {
   const { user } = useUserStorage();
   const { colorScheme } = useColorScheme();
 
-  const hasImage = false;
+  // const uri = 'https://avatars.githubusercontent.com/u/39889384?v=4';
 
-  const uri = 'https://avatars.githubusercontent.com/u/39889384?v=4';
+  const name = `${user?.user_metadata.first_name}+${user?.user_metadata.last_name}`;
+  const uri = `https://ui-avatars.com/api/?name=${name}&background=random`;
+  const fullName = `${user?.user_metadata.first_name} ${user?.user_metadata.last_name}`;
 
   return (
     <View className="mb-5 flex-row items-center justify-between border-b border-gray-300 pb-3 dark:border-gray-400">
       <View className="flex-row items-center gap-x-3">
         <Image
-          source={hasImage ? { uri } : defaultProfile}
+          source={user ? { uri } : defaultProfile}
           className="h-14 w-14 rounded-full border border-gray-800 dark:border-gray-100"
         />
 
         <View>
           <Text className="text-base text-black dark:text-white">Olá,</Text>
-          <Text className="text-lg font-bold text-black dark:text-white">
-            {user?.user_metadata.first_name}
-          </Text>
+          <Text className="text-lg font-bold text-black dark:text-white">{fullName}</Text>
         </View>
       </View>
 
