d7e2c065f711d30aea750f0c8346b65392c24806
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-01 18:46:25 -0300

    refactor: Update AddCardScreen to reset form after submission and navigate back

--- src/screens/App/AddCardScreen.tsx
+++ src/screens/App/AddCardScreen.tsx
@@ -2,19 +2,21 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
 import { ScrollView, View } from 'react-native';
 
+import { AppScreenProps } from '@/Routes';
 import { useAddCreditCard } from '@/api';
 import { BackButton, Box, CustomButton, FormPickerSelect, TextInput } from '@/components';
 import { useUserStorage } from '@/contexts';
 import { addCardSchema, AddCardSchema } from '@/schemas';
 import { formatBirthDate, formatCurrencyOnDigiting, listBanks, parseCurrency } from '@/utils';
 
-export function AddCardScreen() {
+export function AddCardScreen({ navigation }: AppScreenProps<'AddCardScreen'>) {
   const { user } = useUserStorage();
   const { addCard, isPending } = useAddCreditCard();
 
   const {
     control,
     handleSubmit,
+    reset,
     formState: { isDirty, isValid },
   } = useForm<AddCardSchema>({
     resolver: zodResolver(addCardSchema),
@@ -35,6 +37,10 @@ export function AddCardScreen() {
       validity: data.validity,
       limit: parseCurrency(data.limit),
     });
+
+    reset();
+
+    navigation.goBack();
   }
 
   return (
